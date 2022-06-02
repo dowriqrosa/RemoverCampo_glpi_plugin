66dd5df82091456746f6cbb2f490b4f5077fb555
dowriqrosa <carlosh2204@hotmail.com>
2022-06-02 15:03:55 -0300

    ajuste selector não existe em algumas categorias

--- js/js.js
+++ js/js.js
@@ -3,10 +3,14 @@ $(function() {
     let urlCriarChamado = url.split("create_ticket=1")
     let urlCategoria = url.split("tracking.injector.php")
     if(urlCriarChamado.length > 1 || urlCategoria.length > 1){
-        let campoLocalização = document.querySelector("#itil-form > div > div.card-body.row.mx-0 > div > div:nth-child(4)")
-        let contemNomeLocalização = campoLocalização.innerHTML.split("Localização")
-        if(contemNomeLocalização.length <= 1 ){
-            campoLocalização.style.visibility ="hidden"
+        try {
+            let campoLocalização = document.querySelector("#itil-form > div > div.card-body.row.mx-0 > div > div:nth-child(4)")
+            let contemNomeLocalização = campoLocalização.innerHTML.split("Localização")
+            if(contemNomeLocalização.length <= 1 ){
+                campoLocalização.style.visibility ="hidden"
+            }
+        } catch (error) {
+            
         }
         document.querySelector("#itil-form > div > div.card-body.row.mx-0 > div > div:nth-child(1)").style.visibility ="hidden"
         document.querySelector("#itil-form > div > div.card-body.row.mx-0 > div > div:nth-child(3)").style.visibility ="hidden"
